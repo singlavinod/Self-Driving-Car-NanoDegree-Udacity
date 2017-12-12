9e5e08c8811c701c6f6e5dbc7b3e8afd122465a2
Vinod Singla <vsingla2@ncsu.edu>
2017-12-12 00:33:03 -0500

    feat: implement kalman_filter class

--- Term2-Sensor-Fusion-Localization-and-Control/Project1-Extended-Kalman-Filter-Sensor-Fusion/src/kalman_filter.cpp
+++ Term2-Sensor-Fusion-Localization-and-Control/Project1-Extended-Kalman-Filter-Sensor-Fusion/src/kalman_filter.cpp
@@ -1,30 +1,25 @@
 #include "kalman_filter.h"
-
+#include <iostream>
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
-
+using namespace std;
 // Please note that the Eigen library does not initialize 
 // VectorXd or MatrixXd objects with zeros upon creation.
 
-KalmanFilter::KalmanFilter() {}
+KalmanFilter::KalmanFilter() {
+  cout<<"Kalman Filter constructor running\n"<<endl;
+}
 
 KalmanFilter::~KalmanFilter() {}
-
-void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
-                        MatrixXd &H_in, MatrixXd &R_in, MatrixXd &Q_in) {
-  x_ = x_in;
-  P_ = P_in;
-  F_ = F_in;
-  H_ = H_in;
-  R_ = R_in;
-  Q_ = Q_in;
-}
 
 void KalmanFilter::Predict() {
   /**
   TODO:
     * predict the state
   */
+  x_ = F_ * x_;
+  MatrixXd Ft = F_.transpose();
+  P_ = F_ * P_ * Ft + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
@@ -32,6 +27,19 @@ void KalmanFilter::Update(const VectorXd &z) {
   TODO:
     * update the state by using Kalman Filter equations
   */
+  VectorXd z_pred = H_ * x_;
+  VectorXd y = z - z_pred;
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd PHt = P_ * Ht;
+  MatrixXd K = PHt * Si;
+
+  //new estimate
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
@@ -39,4 +47,35 @@ void KalmanFilter::UpdateEKF(const VectorXd &z) {
   TODO:
     * update the state by using Extended Kalman Filter equations
   */
+  VectorXd z_pred(4);
+
+  double rho = sqrt(x_(0)*x_(0) + x_(1)*x_(1));
+  double phi = atan2(x_(1), x_(0)); 
+  double rho_dot;
+  
+  if(fabs(rho) > 0.0001){
+    rho_dot = (x_(0)*x_(2) + x_(1)*x_(3))/rho;
+  }
+  else{
+    rho_dot = 0; 
+  }
+
+  z_pred << rho, phi, rho_dot;
+  
+  VectorXd y = z - z_pred;
+  
+  //Normalize residual phi to be between -pi to pi
+  y(2) = atan2(sin(y(2)), cos(y(2)));
+  
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd PHt = P_ * Ht;
+  MatrixXd K = PHt * Si;
+
+  //new estimate
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_; 
 }
--- Term2-Sensor-Fusion-Localization-and-Control/Project1-Extended-Kalman-Filter-Sensor-Fusion/src/kalman_filter.h
+++ Term2-Sensor-Fusion-Localization-and-Control/Project1-Extended-Kalman-Filter-Sensor-Fusion/src/kalman_filter.h
@@ -34,18 +34,6 @@ public:
   virtual ~KalmanFilter();
 
   /**
-   * Init Initializes Kalman filter
-   * @param x_in Initial state
-   * @param P_in Initial state covariance
-   * @param F_in Transition matrix
-   * @param H_in Measurement matrix
-   * @param R_in Measurement covariance matrix
-   * @param Q_in Process covariance matrix
-   */
-  void Init(Eigen::VectorXd &x_in, Eigen::MatrixXd &P_in, Eigen::MatrixXd &F_in,
-      Eigen::MatrixXd &H_in, Eigen::MatrixXd &R_in, Eigen::MatrixXd &Q_in);
-
-  /**
    * Prediction Predicts the state and the state covariance
    * using the process model
    * @param delta_T Time between k and k+1 in s
