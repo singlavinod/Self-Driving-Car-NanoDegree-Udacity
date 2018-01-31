4637383eef322c075efb8c2269cc2f05850e39a5
Vinod Kumar Singla <vsingla2@ncsu.edu>
2018-01-30 20:42:37 -0500

    fix: Update control gains and comment-out unused variables

--- Term2-Sensor-Fusion-Localization-and-Control/Project4-PID-Controller/src/main.cpp
+++ Term2-Sensor-Fusion-Localization-and-Control/Project4-PID-Controller/src/main.cpp
@@ -34,7 +34,7 @@ int main()
 
   PID pid;
   // TODO: Initialize the pid variable.
-  pid.Init(0.05, 0, 0);
+  pid.Init(0.2, 0.002, 7);
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
@@ -49,8 +49,8 @@ int main()
         if (event == "telemetry") {
           // j[1] is the data JSON object
           double cte = std::stod(j[1]["cte"].get<std::string>());
-          double speed = std::stod(j[1]["speed"].get<std::string>());
-          double angle = std::stod(j[1]["steering_angle"].get<std::string>());
+          // double speed = std::stod(j[1]["speed"].get<std::string>());
+          // double angle = std::stod(j[1]["steering_angle"].get<std::string>());
           double steer_value;
           /*
           * TODO: Calcuate steering value here, remember the steering value is
@@ -59,7 +59,7 @@ int main()
           * another PID controller to control the speed!
           */
           pid.UpdateError(cte);
-          steer_value = deg2rad(angle) - pid.TotalError();
+          steer_value = - pid.TotalError();
 
           if(steer_value > 1) {steer_value = 1;}
           else if(steer_value < -1) {steer_value = -1;}
