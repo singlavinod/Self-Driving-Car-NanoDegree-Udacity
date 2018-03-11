b543c5f6dbf1f17b4489fb0527f6ffea01e891e2
Vinod Kumar Singla <vsingla2@ncsu.edu>
2018-03-11 11:08:19 -0400

    feat: Implement particle filter initialization and prediction methods

--- Term2-Sensor-Fusion-Localization-and-Control/Project3-Kidnapped-Vehicle-Project/src/particle_filter.cpp
+++ Term2-Sensor-Fusion-Localization-and-Control/Project3-Kidnapped-Vehicle-Project/src/particle_filter.cpp
@@ -14,25 +14,83 @@
 #include <sstream>
 #include <string>
 #include <iterator>
+#include <assert.h>
 
 #include "particle_filter.h"
 
 using namespace std;
 
 void ParticleFilter::init(double x, double y, double theta, double std[]) {
-	// TODO: Set the number of particles. Initialize all particles to first position (based on estimates of 
-	//   x, y, theta and their uncertainties from GPS) and all weights to 1. 
+	// Set the number of particles. Initialize all particles to first position (based on estimates of 
+	// x, y, theta and their uncertainties from GPS) and all weights to 1. 
 	// Add random Gaussian noise to each particle.
 	// NOTE: Consult particle_filter.h for more information about this method (and others in this file).
-
+	default_random_engine gen;
+	
+	// Standard deviations for x, y, and theta
+	double std_x, std_y, std_theta;
+	
+	// Ascertain std has 3 values of standard deviations, one each for x, y and theta
+	assert(sizeof(std)/sizeof(std[0]) == 3);
+	
+	// Set standard deviations for x, y, and theta.
+	std_x = std[0];
+	std_y = std[1];
+	std_theta = std[2];
+	
+	// Below lines create normal distributions for x, y and theta.
+	normal_distribution<double> dist_x(gps_x, std_x);
+	normal_distribution<double> dist_y(gps_y, std_y);
+	normal_distribution<double> dist_theta(theta, std_theta);
+		
+	// Set the number of particles
+	num_particles = 50;
+	
+	// Initialize particles 
+	for (int i = 0; i < num_particles; ++i) {
+		double sample_x, sample_y, sample_theta;		
+		// "gen" is the random engine initialized earlier.
+		Particle particle;
+		particle.id = i;
+		particle.x = dist_x(gen);
+		particle.y = dist_y(gen);
+		particle.theta = dist_theta(gen);
+		particle.weight = 1;
+		particles.push_back(particle);
+		weights.push_back(particle.weight);
+	}
+	is_initialized = true;
 }
 
 void ParticleFilter::prediction(double delta_t, double std_pos[], double velocity, double yaw_rate) {
-	// TODO: Add measurements to each particle and add random Gaussian noise.
+	// Add measurements to each particle and add random Gaussian noise.
 	// NOTE: When adding noise you may find std::normal_distribution and std::default_random_engine useful.
-	//  http://en.cppreference.com/w/cpp/numeric/random/normal_distribution
-	//  http://www.cplusplus.com/reference/random/default_random_engine/
-
+	// http://en.cppreference.com/w/cpp/numeric/random/normal_distribution
+	// http://www.cplusplus.com/reference/random/default_random_engine/
+	default_random_engine gen;
+	
+	// Ascertain std_pos has 3 values of standard deviations, one each for x, y and theta
+	assert(sizeof(std_pos)/sizeof(std_pos[0]) == 3);
+	 
+	// Add measurements and gaussian noise to each particle
+	for (int i = 0; i < num_particles; ++i)
+	{
+		// Add measurements to ith particle
+		double theta0 = particles[i].theta;
+		particles[i].theta += yaw_rate*delta_t;
+		particles[i].x += (velocity/yaw_rate)*(sin(particles[i].theta) - sin(theta0));
+		particles[i].y += (velocity/yaw_rate)*(cos(theta0) - cos(particles[i].theta));
+		
+		// Below lines create normal distributions for x, y and theta.
+		normal_distribution<double> dist_x(particles[i].x, std_pos[0]);
+		normal_distribution<double> dist_y(particles[i].y, std_pos[1]);
+		normal_distribution<double> dist_theta(particles[i].theta, std_pos[2]);
+		
+		// Add noise to each particle
+		particles[i].x += dist_x(gen);
+		particles[i].y += dist_y(gen);
+		particles[i].theta += dist_theta(gen);
+	}
 }
 
 void ParticleFilter::dataAssociation(std::vector<LandmarkObs> predicted, std::vector<LandmarkObs>& observations) {
