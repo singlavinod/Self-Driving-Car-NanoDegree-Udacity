2c6f9a166909b9c2a83cc13ab52a6309c51bc90d
Vinod Singla <vsingla2@ncsu.edu>
2019-01-13 14:48:39 -0500

    feat: implement load_vgg function

--- Term3-Path-Planning-Concentrations-and-Systems/Project2-Semantic-Segmentation-Elective/main.py
+++ Term3-Path-Planning-Concentrations-and-Systems/Project2-Semantic-Segmentation-Elective/main.py
@@ -33,8 +33,18 @@ def load_vgg(sess, vgg_path):
     vgg_layer3_out_tensor_name = 'layer3_out:0'
     vgg_layer4_out_tensor_name = 'layer4_out:0'
     vgg_layer7_out_tensor_name = 'layer7_out:0'
-    
-    return None, None, None, None, None
+
+    # load model
+    tf.saved_model.loader.load(sess, {vgg_tag}, vgg_path)
+    graph = tf.get_default_graph()
+    # load tensors (image input, keep prob, layers) by name
+    image_input = graph.get_tensor_by_name(vgg_input_tensor_name)
+    keep_prob = graph.get_tensor_by_name(vgg_keep_prob_tensor_name)
+    layer3_out = graph.get_tensor_by_name(vgg_layer3_out)
+    layer4_out = graph.get_tensor_by_name(vgg_layer4_out)
+    layer7_out = graph.get_tensor_by_name(vgg_layer7_out)
+
+    return image_input, keep_prob, layer3_out, layer4_out, layer7_out
 tests.test_load_vgg(load_vgg, tf)
 
 
